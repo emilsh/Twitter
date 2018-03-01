9d4a07298800074155438eabb42e459887ab7a88
developer <developer@developers-Mac.local>
2018-03-01 02:45:25 -0800

    start implementing custom nav bar

--- Twitter/Controller/HomeDatasourceController.swift
+++ Twitter/Controller/HomeDatasourceController.swift
@@ -13,8 +13,14 @@ class HomeDatasourceController: DatasourceController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupNavigationBarItems()
+        
         let homeDatasource = HomeDatasource()
         self.datasource = homeDatasource
+    }
+    
+    private func setupNavigationBarItems() {
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
