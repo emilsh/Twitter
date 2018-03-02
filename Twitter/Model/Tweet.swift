1c017ffc04c6d2615ce28a3c30c73829a0d35ca4
emilsh <shafigin@gmail.com>
2018-03-02 20:37:29 +0600

    Implementing tweets string formatting

---
+++ Twitter/Model/Tweet.swift
@@ -0,0 +1,14 @@
+//
+//  Tweet.swift
+//  Twitter
+//
+//  Created by Emil Shafigin on 3/2/18.
+//  Copyright © 2018 emksh. All rights reserved.
+//
+
+import Foundation
+
+struct Tweet {
+    let user: User
+    let message: String
+}
--- Twitter/View/TweetCell.swift
+++ Twitter/View/TweetCell.swift
@@ -10,12 +10,37 @@ import LBTAComponents
 
 class TweetCell: DatasourceCell {
     
+    override var datasourceItem: Any? {
+        didSet {
+            guard let tweet = datasourceItem as? Tweet else { return }
+            
+            let attributedText = NSMutableAttributedString(string: tweet.user.name, attributes: [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 16)])
+            
+            let usernameString = "  \(tweet.user.username)\n"
+            attributedText.append(NSAttributedString(string: usernameString, attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 15), NSAttributedStringKey.foregroundColor: UIColor.gray]))
+            
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.lineSpacing = 10
+            let range = NSMakeRange(0, attributedText.string.count)
+            attributedText.addAttributes([NSAttributedStringKey.paragraphStyle: paragraphStyle], range: range)
+            
+            attributedText.append(NSAttributedString(string: tweet.message, attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 15)]))
+            
+            messageTextView.attributedText = attributedText
+        }
+    }
+    
     let profileImage: UIImageView = {
         let image = UIImageView()
         image.image = #imageLiteral(resourceName: "profile_image")
         image.layer.cornerRadius = 5
         image.clipsToBounds = true
         return image
+    }()
+    
+    let messageTextView: UITextView = {
+        let tv = UITextView()
+        return tv
     }()
     
     override func setupViews() {
@@ -27,6 +52,9 @@ class TweetCell: DatasourceCell {
         backgroundColor = .white
         
         addSubview(profileImage)
+        addSubview(messageTextView)
         profileImage.anchor(self.topAnchor, left: self.leftAnchor, bottom: nil, right: nil, topConstant: 12, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 50, heightConstant: 50)
+        
+        messageTextView.anchor(topAnchor, left: profileImage.rightAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 4, leftConstant: 4, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
     }
 }
